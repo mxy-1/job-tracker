ff26602ed3beae2ab14437279ce4a550a9fad731
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-04-11 11:17:10 +0100

    add env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
+
 const express = require("express")
 const app = express()
-const port = 3000
 const applications = require("./routes/applications")
 
 app.get("/", (req, res) => {
@@ -9,25 +10,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/applications", applications)
 
-// app.route("/api/applications")
-//     .get((req, res) => {
-//         res.send("GET applications")
-//     })
-//     .post((req, res) => {
-//         res.send("POST applications")
-//     })
-
-// app.route("/api/applications/:id")
-//     .get((req, res) => {
-//         res.send("GET application by id")
-//     })
-//     .put((req, res) => {
-//         res.send("PUT application by id")
-//     })
-//     .delete((req, res) => {
-//         res.send("DELETE application by id")
-//     })
-
-app.listen(port, () => {
-    console.log("port listening on", port)
+app.listen(process.env.PORT, () => {
+    console.log("port listening on", process.env.PORT)
 })
