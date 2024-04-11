acde9fdeb127d8299ab516ed72233bf8aed6ada3
May Hoang <mayhoang@hotmail.co.uk>
2024-04-11 19:22:55 +0100

    fetch job app data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
+type ApplicationType = {
+  job_title: string
+  company: string
+}
+
 function App() {
-  fetch("http://localhost:4000/api/applications/")
-    .then((res) => {
-      return res.json()
-    })
-    .then(data => {
-      console.log(data)
-    })
+  const [applications, setApplications] = useState<null | ApplicationType[]>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/applications/")
+      .then((res) => {
+        return res.json()
+      })
+      .then(data => {
+        setApplications(data.applications)
+      })
+  }, []);
+
 
   return (
-    <h1>Job tracker</h1>
+    <div>
+      <h1>Job tracker</h1>
+      <div>
+        {applications && applications.map(application => (
+          <div>
+            <h2>{application.job_title}</h2>
+            <h3>{application.company}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
