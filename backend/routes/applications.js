6dfae71040de03dafcaded2296bd08476756a42d
May Hoang <mayhoang@hotmail.co.uk>
2024-04-11 12:22:43 +0100

    add schema

--- backend/routes/applications.js
+++ backend/routes/applications.js
@@ -1,11 +1,23 @@
 const express = require("express")
+const Application = require("../models/applications.models")
 const router = express.Router()
 
 router.get("/", (req, res) => {
     res.send("GET applications")
 })
-router.post("/",(req, res) => {
-    res.send("POST applications")
+router.post("/", async (req, res) => {
+    const { company, job_title } = req.body
+
+    try {
+        const application = await Application.create({ company, job_title })
+        res.status(200).send({application})
+        console.log("added")
+
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+        console.log("error", error)
+
+    }
 })
 
 router.get("/:id", (req, res) => {
