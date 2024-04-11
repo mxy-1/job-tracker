fb60a2a1a660ce5bbb910219b05cca7f783c7dd5
May Hoang <mayhoang@hotmail.co.uk>
2024-04-11 11:39:28 +0100

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 require("dotenv").config()
 
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 const applications = require("./routes/applications")
 
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
+// app.get("/", (req, res) => {
+//     res.send("hello world")
+// })
+
+app.use(express.json()) // parses any data
 
 app.use("/api/applications", applications)
 
-app.listen(process.env.PORT, () => {
-    console.log("port listening on", process.env.PORT)
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("connected")
+        // start listening to db once connection established
+        app.listen(process.env.PORT, () => {
+            console.log("port listening on", process.env.PORT)
+        })
+    })
+    .catch((error) => console.log(error))
+
