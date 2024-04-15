1976fe4acc7109c2562702554b682d7bcd6a3458
May Hoang <mayhoang@hotmail.co.uk>
2024-04-15 21:14:29 +0100

    add icons

--- frontend/src/components/Application.tsx
+++ frontend/src/components/Application.tsx
@@ -1,6 +1,9 @@
 import { ApplicationType } from "../types/Application.type";
 import "./Application.styles.css"
 import { formatDate } from "../utils";
+import { IoLocationOutline } from "react-icons/io5";
+import { GoClock } from "react-icons/go";
+import { PiCoinLight } from "react-icons/pi";
 
 const Application = ({ application }: { application: ApplicationType }) => {
 
@@ -20,10 +23,10 @@ const Application = ({ application }: { application: ApplicationType }) => {
             <h2 className="job-title">{application.jobTitle}</h2>
             <h3 className="company">{application.company}</h3>
 
-            <p>{application.location}</p>
+            <p><IoLocationOutline size={14} color="#006884"/> {application.location}</p>
 
-            {application.salary && <p className="salary">£{application.salary} </p>}
-            {application.hours && <p className="hours">{application.hours} hrs/week</p>}
+            {application.salary && <p className="salary"><PiCoinLight size={13} color="#006884"/> £{application.salary} </p>}
+            {application.hours && <p className="hours"><GoClock size={13} color="#006884"/> {application.hours} hrs/week</p>}
 
             <p className="comments">{application.comments}</p>
 
