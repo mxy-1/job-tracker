5f8fb4750ae131ce9e4d3041218abf78229af9a7
May Hoang <mayhoang@hotmail.co.uk>
2024-04-18 16:07:01 +0100

    get form data and update state

--- frontend/src/components/ApplicationForm.tsx
+++ frontend/src/components/ApplicationForm.tsx
@@ -1,35 +1,63 @@
+import { useState } from "react";
+import { ApplicationDataType } from "../types/ApplicationData.type";
 import "./ApplicationForm.style.css"
+import { getCurrentDate } from "../utils";
 
 const ApplicationForm = () => {
+
+    const initialApplicationData = {
+        date: getCurrentDate(),
+        company: "",
+        title: "",
+        location: "",
+        hours: "",
+        salary: "",
+        deadline: "",
+        status: "Waiting to hear back",
+        interview: "",
+        comments: ""
+    }
+
+    const [applicationData, setApplicationData] = useState<ApplicationDataType>(initialApplicationData);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>):void => {
+        const value = e.target.value
+        const name = e.target.name
+
+        setApplicationData((prevApplicationData) => {
+            return {...prevApplicationData, [name]: value}
+        })
+    }
+
     return (
         <div className="application-container">
             <h2>Job application</h2>
 
             <form>
                 <label>
-                    Date <input type="date" />
+                    Date <input type="date" id="date" name="date" value={applicationData.date} onChange={handleChange}/>
                 </label>
                 <label>
-                    Company <input type="text" />
+                    Company <input type="text" name="company" value={applicationData.company}  onChange={handleChange}/>
                 </label>
                 <label>
-                    Job title <input type="text" required />
+                    Job title <input type="text" name="title" required value={applicationData.title}  onChange={handleChange}/>
                 </label>
                 <label>
-                    Location <input type="string" />
+                    Location <input type="string" name="location" value={applicationData.location}  onChange={handleChange}/>
                 </label>
                 <label>
-                    Hours/week <input type="number" min={0} max={100} />
+                    Hours/week <input type="number" name="hours" min={0} max={100} value={applicationData.hours}  onChange={handleChange}/>
                 </label>
                 <label>
-                    Salary (£/year) <input type="number" min={0}/>
+                    Salary (£/year) <input type="number" name="salary" min={0} value={applicationData.salary}  onChange={handleChange}/>
                 </label>
                 <label>
-                    Deadline <input type="date" />
+                    Deadline <input type="date" name="deadline" value={applicationData.deadline}  onChange={handleChange}/>
                 </label>
                 <label>
                     Status
-                    <select>
+                    <select name="status" value={applicationData.status} onChange={handleChange}>
                         <option>Waiting to hear back</option>
                         <option>Interviewing</option>
                         <option>Unsuccessful</option>
@@ -41,10 +69,10 @@ const ApplicationForm = () => {
                 </label>
 
                 <label>
-                    Interview <input type="date" />
+                    Interview <input type="date" value={applicationData.interview}  onChange={handleChange}/>
                 </label>
                 <label>
-                    Comments <textarea rows={4}/>
+                    Comments <textarea rows={4} value={applicationData.comments}  onChange={handleChange}/>
                 </label>
                 <button type="submit">Add</button>
             </form>
