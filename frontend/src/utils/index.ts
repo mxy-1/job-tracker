fa503efb4bc4fd3c62f3ffa0d82d7b0e2f65cef1
May Hoang <mayhoang@hotmail.co.uk>
2024-04-18 16:06:13 +0100

    get current day formatted

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -5,3 +5,11 @@ export const formatDate = (dateString: string):string => {
     const year = date.getUTCFullYear().toString().slice(2)
     return `${day}/${month}/${year}`
 }
+
+export const getCurrentDate = ():string => {
+    const d = new Date()
+    const day = d.getDate().toString().padStart(2, "0")
+    const month = (d.getMonth() + 1).toString().padStart(2, "0")
+    const year = d.getFullYear()
+    return `${year}-${month}-${day}`
+}
