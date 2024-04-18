124c8e6850b13898ff0ad3784546f6b4c390cdb4
May Hoang <mayhoang@hotmail.co.uk>
2024-04-18 16:05:48 +0100

    add type for inital form data

---
+++ frontend/src/types/ApplicationData.type.ts
@@ -0,0 +1,12 @@
+export type ApplicationDataType = {
+    date: string
+    company: string
+    title: string
+    location: string
+    hours: number | string
+    salary: number | string
+    deadline: string
+    status: string,
+    interview: string
+    comments: string
+}
