6d6e7b1fdedbbba3e76209e32335d05a44ac797b
May Hoang <mayhoang@hotmail.co.uk>
2024-04-24 13:04:19 +0100

    validate signup details

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,13 +1,22 @@
 const User = require("../models/user.model")
+const bcrypt = require("bcrypt")
 
 // login
 const loginUser = async (req, res) => {
-    res.json({msg: "login user"})
+    res.json({ msg: "login user" })
 }
+
 
 // signup
 const signupUser = async (req, res) => {
-    res.json({msg: "signup user"})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+        res.status(201).send({user})
+    } catch (error) {
+        res.status(400).send({error: error.message})
+    }
 }
 
-module.exports = {loginUser, signupUser}
+module.exports = { loginUser, signupUser }
