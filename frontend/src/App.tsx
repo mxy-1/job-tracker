f969d703dd44c6984b41a2d6e6838ab6084d3659
May Hoang <mayhoang@hotmail.co.uk>
2024-05-08 16:24:20 +0100

    update routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,23 +4,13 @@ import { Nav } from './components/Nav';
 import { Jobs } from './pages/Jobs';
 import { Form } from './pages/Form';
 import { Header } from './components/Header';
-import LogIn from './pages/LogIn';
+import LogIn from './pages/Login';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Header />
-      {/* <Routes>
-        <Route path={"/jobs"} element={<Nav/>}>
-          <Route index element={<Jobs/>}/>
-          <Route path="add-new" element={<Form/>}/>
-          <Route path=":id" element={<Form />} />
-        </Route>
-        <Route path="/sign-up" element={<LogIn />}>
-        </Route>
-      </Routes> */}
-
       <Routes>
         <Route path="jobs">
           <Route index element={<Jobs/>}/>
