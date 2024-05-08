4795eb665700432114a93f8484506b0c73bf5769
May Hoang <mayhoang@hotmail.co.uk>
2024-05-08 16:00:28 +0100

    update button style

---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "./LoginForm.style.css"
+import { useSignUp } from "../hooks/useSignUp"
+
+const LoginForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [login, setLogin] = useState(true);
+
+    const {signUp, error, isLoading} = useSignUp()
+
+    const handleSubmit = async (e: React.SyntheticEvent) => {
+        e.preventDefault()
+        await signUp(email, password)
+    }
+
+    const handleClick = () => {
+        setLogin(prevState => !prevState)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h2>{login ? "Log In" : "Sign up"}</h2>
+                <label>Email:
+                    <input
+                        type="email"
+                        onChange={e => setEmail(e.target.value)} />
+                </label>
+                <label>Password:
+                    <input
+                        type="password"
+                        onChange={e => setPassword(e.target.value)} />
+                </label>
+                <button disabled={isLoading} type="submit" className={isLoading? "disabled submit" : "submit"}>{login ? "Log in" : "Sign up"}</button>
+
+            </form>
+            {
+                login ?
+                    <p className='account'>Don't have an account? <Link to="/sign-up" className='login' onClick={handleClick}>Sign up</Link></p>
+                    : <p className='account'>Already have an account? <Link to="/log-in" className='login' onClick={handleClick}>Log in</Link></p>
+            }
+            {error && <p>{error}</p>}
+        </div>
+    );
+}
+
+export default LoginForm;
