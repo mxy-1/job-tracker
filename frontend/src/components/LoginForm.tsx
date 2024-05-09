62f71d9c121031f0c4f5148ec8aeb18a604ca3c2
May Hoang <mayhoang@hotmail.co.uk>
2024-05-09 16:11:50 +0100

    resolve type errors

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 import "./LoginForm.style.css"
 import { useSignUp } from "../hooks/useSignUp"
 import { useLogOut } from '../hooks/useLogOut';
+import { useAuthContext } from '../hooks/useAuthContext';
 // import useLogIn from '../hooks/useLogIn';
 
 const LoginForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [login, setLogin] = useState(true);
+    const { user } = useAuthContext()
 
-    const {signUp, error, isLoading} = useSignUp()
-    const {logOut} = useLogOut()
+    const { signUp, error, isLoading } = useSignUp()
+    const { logOut } = useLogOut()
     // const {logIn, error, isLoading} = useLogIn()
 
     const handleSubmit = async (e: React.SyntheticEvent) => {
@@ -32,30 +34,41 @@ const LoginForm = () => {
         logOut()
     }
 
+
+
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <h2>{login ? "Log In" : "Sign up"}</h2>
-                <label>Email:
-                    <input
-                        type="email"
-                        onChange={e => setEmail(e.target.value)} />
-                </label>
-                <label>Password:
-                    <input
-                        type="password"
-                        onChange={e => setPassword(e.target.value)} />
-                </label>
-                <button disabled={isLoading} type="submit" className="submit">{login ? "Log in" : "Sign up"}</button>
+            {!user ?
+                <>
+                    <form onSubmit={handleSubmit}>
+                        <h2>{login ? "Log In" : "Sign up"}</h2>
+                        <label>Email:
+                            <input
+                                type="email"
+                                onChange={e => setEmail(e.target.value)} 
+                                autoComplete='email'/>
+                        </label>
+                        <label>Password:
+                            <input
+                                type="password"
+                                onChange={e => setPassword(e.target.value)} />
+                        </label>
+                        <button disabled={isLoading} type="submit" className="submit">{login ? "Log in" : "Sign up"}</button>
 
-            </form>
-            {
-                login ?
-                    <p className='account'>Don't have an account? <Link to="/sign-up" className='login' onClick={handleClick}>Sign up</Link></p>
-                    : <p className='account'>Already have an account? <Link to="/log-in" className='login' onClick={handleClick}>Log in</Link></p>
-            }
-            {error && <p className='error'>{error}</p>}
-            <button className='submit' onClick={handleLogOut}>Log Out</button>
+                    </form>
+                    {
+                        login ?
+                            <p className='account'>Don't have an account? <Link to="/sign-up" className='login' onClick={handleClick}>Sign up</Link></p>
+                            : <p className='account'>Already have an account? <Link to="/log-in" className='login' onClick={handleClick}>Log in</Link></p>
+                    }
+                    {error && <p className='error'>{error}</p>}
+                </>
+
+                :
+                <>
+                    <h2>{user.email}</h2>
+                    <button className='submit' onClick={handleLogOut}>Log Out</button>
+                </>}
         </div>
     );
 }
--- frontend/src/types/Auth.type.ts
+++ frontend/src/types/Auth.type.ts
@@ -1,12 +1,29 @@
+// export type AuthType = {
+//     user: string | null
+// }
+
+// export type ActionType = {
+//     type: "LOGIN" | "LOGOUT"
+//     payload: string
+// } 
+
+// export type AuthContextType = {
+//     dispatch: React.Dispatch<ActionType>
+// }
+type UserType = {
+    email: string
+    password: string
+}
 export type AuthType = {
-    user: string | null
+    user: UserType | null
 }
 
 export type ActionType = {
     type: "LOGIN" | "LOGOUT"
-    payload: string
+    payload: UserType| null
 } 
 
 export type AuthContextType = {
     dispatch: React.Dispatch<ActionType>
+    user: UserType | null
 }
