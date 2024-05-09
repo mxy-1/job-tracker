fc4564bdedeb2dfc383de265eb2e6da34d4f7b80
May Hoang <mayhoang@hotmail.co.uk>
2024-05-09 11:35:28 +0100

    logout user

---
+++ frontend/src/hooks/useLogOut.ts
@@ -0,0 +1,16 @@
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogOut = () => {
+    const {dispatch} = useAuthContext()
+
+    const logOut = () => {
+        // remove user from storage
+        localStorage.removeItem("user")
+
+        //dispatch logout action
+        dispatch({type: "LOGOUT", payload: ""})
+    }
+
+    return {logOut}
+}
+ 
