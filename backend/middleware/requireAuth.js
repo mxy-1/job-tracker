44f253c861af0cf2063eae4a5e292a2901cccff7
May Hoang <mayhoang@hotmail.co.uk>
2024-05-10 10:26:28 +0100

    middleware to protect api routes

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,3 +1,27 @@
-const requireAuth = (req, res, next) => {
-    
+const jwt = require("jsonwebtoken")
+const User = require("../models/user.model")
+
+const requireAuth = async (req, res, next) => {
+    // verify authentication - contains web token
+    const {authorization} = req.headers
+
+    if (!authorization) {
+        return res.status(401).send({error: "Authorization token required"})
+    }
+
+    const token = authorization.split(" ")[1]
+
+    try {
+        const {_id} = jwt.verify(token, process.env.SECRET)
+
+        // add user key to req
+        req.user = await User.findById(_id).select("_id")
+        next()
+
+    } catch (error) {
+        console.log(error)
+        res.status(401).send({error: "Request is not authorized"})
+    }
 }
+
+module.exports = requireAuth
--- backend/routes/applications.js
+++ backend/routes/applications.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const { postApplication, getAllApplications, getApplicationById, deleteApplicationById, patchApplicationById } = require("../controllers/applications.controller")
+const requireAuth = require("../middleware/requireAuth")
+
 const router = express.Router()
+
+// authenticates users before other routes
+router.use(requireAuth)
 
 router.get("/", getAllApplications)
 
