224d962292b01266a52f18e50ef0538fb7fd7c6b
May Hoang <mayhoang@hotmail.co.uk>
2024-06-23 22:01:16 +0100

    use location

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,9 +1,11 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
     const isLoggedIn = !!localStorage.getItem('user');
+    const location = useLocation()
 
-    return  isLoggedIn ? <>{children}</> : <Navigate to="/log-in" />;
+    // pass in current location, so that users can be redirected after login
+    return  isLoggedIn ? <>{children}</> : <Navigate to="/log-in" state={{prev:location.pathname}}/>;
 }
 
 export default PrivateRoute;
